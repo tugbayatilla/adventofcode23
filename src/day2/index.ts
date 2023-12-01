b64644c64877bbb194b9abc5eec7c93164b40800
Tugbay Atilla <tatilla@igus.net>
2023-12-01 15:08:04 +0100

    feat(d1p2): implement handling of string representation of numbers

--- src/day2/index.ts
+++ src/day2/index.ts
@@ -1,16 +1,14 @@
 import { read } from "../read";
 
 export const Day = "day2"; // <-- change this when you copy
-export const SourceFolderPath = `./src/${Day}/`
+export const SourceFolderPath = `./src/${Day}/`;
 
 export function sum(input: string[]): number {
-  
   let sum: number = 0;
 
   for (const line of input) {
-
     let numberString = getNumberAsString([...line]);
-    numberString += getNumberAsString([...line].reverse());
+    numberString += getNumberAsString([...line].reverse(), true);
 
     sum += Number(numberString);
   }
@@ -18,19 +16,50 @@ export function sum(input: string[]): number {
   return sum;
 }
 
-function getNumberAsString(line: string[]): string {
+const stringRepresentationOfNumbers: string[] = [
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+];
+
+function getNumberAsString(
+  line: string[],
+  isReversed: boolean = false
+): string {
+  let accumulatedString: string = "";
+
   for (const p of line) {
     if (!isNaN(Number(p))) {
       return String(p);
     }
-  }
-  throw Error('no number found')
-}
 
+    accumulatedString += p;
+
+    const searchString = isReversed
+      ? [...accumulatedString].reverse().join("")
+      : accumulatedString;
+
+    const index = stringRepresentationOfNumbers.findIndex((element) =>
+      searchString.includes(element)
+    );
+
+    if (index !== -1) {
+      return String(index + 1);
+    }
+  }
+
+  throw Error("no number found");
+}
 
 export async function answer(filePath: string): Promise<number> {
   return read(filePath).then((lines) => sum(lines));
 }
 
-answer(`${SourceFolderPath}puzzle.data`)
-.then(answer=>console.log(answer))
+answer(`${SourceFolderPath}puzzle.data`);
+// .then(answer=>console.log(answer))
