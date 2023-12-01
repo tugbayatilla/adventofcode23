758316d73b7ee8b2325aefdea6e747d48f5d965e
Tugbay Atilla <tatilla@igus.net>
2023-12-01 15:08:56 +0100

    tests(d1p2): uncomment all calibration tests

--- src/day2/index.spec.ts
+++ src/day2/index.spec.ts
@@ -4,12 +4,12 @@ import { sum, answer, SourceFolderPath, Day } from ".";
 describe(`${Day}: sum of all of the calibration values`, () => {
   const theories: [string, number][] = [
     ["two1nine", 29],
-    // ["eightwothree", 83],
-    // ["abcone2threexyz", 13],
-    // ["xtwone3four", 24],
-    // ["4nineeightseven2", 42],
-    // ["zoneight234", 14],
-    // ["7pqrstsixteen", 76],
+    ["eightwothree", 83],
+    ["abcone2threexyz", 13],
+    ["xtwone3four", 24],
+    ["4nineeightseven2", 42],
+    ["zoneight234", 14],
+    ["7pqrstsixteen", 76],
   ];
 
   theories.forEach(([line, expected]) => {
