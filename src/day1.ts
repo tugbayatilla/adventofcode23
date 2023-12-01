3b005c73af904606cb0019b8294c958f3e7f5a8a
Tugbay Atilla <tatilla@igus.net>
2023-12-01 12:08:10 +0100

    feat(d1p1): implement sum function

--- src/day1.ts
+++ src/day1.ts
@@ -1,3 +1,21 @@
 export function sum(input: string[]): number {
-  return 0;
+  const line = input[0];
+  let numberString = "";
+
+  for (const p of [...line]) {
+    if(!isNaN(Number(p))){
+      numberString += String(p)
+      break;
+    }
+  }
+
+  for (const p of [...line].reverse()) {
+    if(!isNaN(Number(p))){
+      numberString += String(p)
+      break;
+    }
+  }
+  
+  return Number(numberString);
+  
 }
