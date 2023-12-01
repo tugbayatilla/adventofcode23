fd4e648131a4c2f2fff9a8b826f1e3634875cc64
Tugbay Atilla <tatilla@igus.net>
2023-12-01 06:34:58 +0100

    refactor: clean-up unnecessary code

--- src/index.spec.ts
+++
@@ -1,8 +0,0 @@
-import { expect } from 'chai';
-import { add } from './index'
-
-describe('add function', () => {
-    it('should 1 + 2 returns 3', () => {
-        expect(add(1, 2)).to.equal(3);
-    })
-});
