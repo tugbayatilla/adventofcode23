1d760cbb00c9e41a539c0e3a698373d64aa2cc6f
Tugbay Atilla <tugbayatilla@gmail.com>
2023-12-02 13:36:45 +0100

    refactor(d2p1): introduce game interface to evaluate function

---
+++ src/day2/part1/game.ts
@@ -0,0 +1,6 @@
+export interface Game {
+    id: number,
+    red?: number | null,
+    green?: number| null,
+    blue?: number | null
+}
--- src/day2/part1/index.ts
+++ src/day2/part1/index.ts
@@ -1,7 +1,9 @@
+import { Game } from "./game";
+
 export const Day = "day2"; // <-- change this when you copy
 export const SourceFolderPath = `./src/${Day}/part1/`;
 
-export function evaluate(game: { id: number; red?: number | null; green?: number, blue?: number }): number {
+export function evaluate(game: Game): number {
   if (game.red !== null && game.red! <= 12) return game.id;
   if (game.green !== null && game.green! <= 13) return game.id;
   if (game.blue !== null && game.blue! <= 14) return game.id;
