22ba5049c44c0ccaef8e6cc6840cb2d611f9211e
Tugbay Atilla <tugbayatilla@gmail.com>
2023-12-03 13:17:37 +0100

    refactor(d2p1): remove parse function and transfer tests to parseLine

--- src/day2/part1/index.spec.ts
+++ src/day2/part1/index.spec.ts
@@ -1,47 +1,28 @@
 import { expect } from "chai";
-import { Day, SourceFolderPath, answer, evaluateSession, parse, sum  } from ".";
+import { Day, SourceFolderPath, answer, evaluateSession, parseLine, sum  } from ".";
 
 
 describe(`${Day}: parse a line`, () => {
   const testLine = 'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green';
 
   it("should the size of the result array be 3", () => {
-    expect(parse(testLine).length).to.equal(3);
+    expect(parseLine(testLine).sessions?.length).to.equal(3);
   }),
-  it("should all ids be 1", () => {
-    expect(parse(testLine))
-    .to
-    .be
-    .an('array')
-    .that
-    .satisfies((arr: {id:number}[]) => (arr).every(obj => obj.id === 1));
+  it("should id be 1", () => {
+    expect(parseLine(testLine).id).to.equal(1);
   }),
-  it("should all ids be 2", () => {
+  it("should id be 2", () => {
     const testLineOverriden = 'Game 2: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green';
-    expect(parse(testLineOverriden))
-    .to
-    .be
-    .an('array')
-    .that
-    .satisfies((arr: {id:number}[]) => (arr).every(obj => obj.id === 2));
-  }),
-  it("should all ids be 1122", () => {
-    const testLineOverriden = 'Game 1122: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green';
-    expect(parse(testLineOverriden))
-    .to
-    .be
-    .an('array')
-    .that
-    .satisfies((arr: {id:number}[]) => (arr).every(obj => obj.id === 1122));
+    expect(parseLine(testLineOverriden).id).to.equal(2);
   }),
   it("should first item have 3 blue", () => {
-    expect(parse(testLine)[0].blue).to.equal(3);
+    expect(parseLine(testLine).sessions![0].blue).to.equal(3);
   }),
   it("should first item have 4 red", () => {
-    expect(parse(testLine)[0].red).to.equal(4);
+    expect(parseLine(testLine).sessions![0].red).to.equal(4);
   }),
   it("should second item have 2 green", () => {
-    expect(parse(testLine)[1].green).to.equal(2);
+    expect(parseLine(testLine).sessions![1].green).to.equal(2);
   })
 });
 
--- src/day2/part1/index.ts
+++ src/day2/part1/index.ts
@@ -11,55 +11,6 @@ export function evaluateSession(session: Session): boolean {
     ((session.blue === undefined) || session.blue! <= 14);
 }
 
-export function parse(line: string): Game[] {
-  let games: Game[] = [];
-
-  let rawData = line.split(';');
-
-  // find game id
-  // the number between space and : at the first element
-  var spaceIndex = rawData[0].indexOf(' ');
-  var colonIndex = rawData[0].indexOf(':');
-  var id = Number(rawData[0].substring(spaceIndex, colonIndex));
-
-  rawData.forEach((p) => {
-    let game: Game = { id: id, sessions: [] };
-    let session: Session = {};
-
-    // find blue
-    const regexBlue = /(\d+)\s+blue/;
-    const matchBlue = regexBlue.exec(p);
-
-    if (matchBlue && matchBlue[1]) {
-      game.blue = parseInt(matchBlue[1], 10);
-      session.blue = parseInt(matchBlue[1], 10);
-    }
-
-    // find red
-    const regexRed = /(\d+)\s+red/;
-    const matchRed = regexRed.exec(p);
-
-    if (matchRed && matchRed[1]) {
-      game.red = parseInt(matchRed[1], 10);
-      session.red = parseInt(matchRed[1], 10);
-    }
-
-    // find green
-    const regexGreen = /(\d+)\s+green/;
-    const matchGreen = regexGreen.exec(p);
-
-    if (matchGreen && matchGreen[1]) {
-      game.green = parseInt(matchGreen[1], 10);
-      session.green = parseInt(matchGreen[1], 10);
-    }
-
-    game.sessions?.push(session);
-    games.push(game);
-  });
-
-  return games;
-}
-
 export function parseLine(line: string): Game {
 
   let rawSessions = line.split(';');
