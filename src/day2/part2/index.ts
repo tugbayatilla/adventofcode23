3d2a1064562069dc614f2f9f32aa08500aceb976
Tugbay Atilla <tugbayatilla@gmail.com>
2023-12-03 16:25:50 +0100

    feat(d2p2): fewest number of cubes of each color

--- src/day2/part2/index.ts
+++ src/day2/part2/index.ts
@@ -3,6 +3,21 @@ import { Session } from "../part1/game";
 export const Day = "day2"; // <-- change this when you copy
 export const SourceFolderPath = `./src/${Day}/part2/`; // <-- change this when you copy
 
-export function findFewest(sessions: Session[]): NonNullable<Session> {
-  return {};
+export function findFewest(sessions: Session[]): Required<Session> {
+  let resultSession: Required<Session> = { red:0, green:0, blue: 0 };
+
+  sessions.forEach(session=>{
+
+    Object.keys(session).forEach(key => {
+        const valueOfKey = session[key as keyof Session] ?? 0;
+
+        if(resultSession[key as keyof Session]! < valueOfKey)
+        {
+          resultSession[key as keyof Session] = valueOfKey;
+        }
+    });
+
+  });
+
+  return resultSession;
 }
