4d983f60f2b3331b3e3d7187aeb94ec127728d0a
Tugbay Atilla <tugbayatilla@gmail.com>
2023-12-03 13:04:51 +0100

    refactor(d2p1): use sessions in sum iwth new parseLine func

--- src/day2/part1/index.ts
+++ src/day2/part1/index.ts
@@ -22,14 +22,14 @@ export function evaluate(game: Game): number {
 }
 
 export function evaluateSession(session: Session): boolean {
-  
+
   return ((session.red === undefined) || session.red! <= 12) &&
     ((session.green === undefined) || session.green! <= 13) &&
     ((session.blue === undefined) || session.blue! <= 14);
 }
 
 export function parse(line: string): Game[] {
-  let result: Game[] = [];
+  let games: Game[] = [];
 
   let rawData = line.split(';');
 
@@ -40,7 +40,7 @@ export function parse(line: string): Game[] {
   var id = Number(rawData[0].substring(spaceIndex, colonIndex));
 
   rawData.forEach((p) => {
-    let game: Game = { id: id };
+    let game: Game = { id: id, sessions: [] };
     let session: Session = {};
 
     // find blue
@@ -71,32 +71,74 @@ export function parse(line: string): Game[] {
     }
 
     game.sessions?.push(session);
-    result.push(game);
+    games.push(game);
   });
 
-  return result;
+  return games;
+}
+
+export function parseLine(line: string): Game {
+
+  let rawSessions = line.split(';');
+
+  // find game id
+  // the number between space and : at the first element
+  var spaceIndex = rawSessions[0].indexOf(' ');
+  var colonIndex = rawSessions[0].indexOf(':');
+  var id = Number(rawSessions[0].substring(spaceIndex, colonIndex));
+
+  let game: Game = { id: id, sessions: [] };
+
+  rawSessions.forEach((p) => {
+
+    let session: Session = {};
+
+    // find blue
+    const regexBlue = /(\d+)\s+blue/;
+    const matchBlue = regexBlue.exec(p);
+
+    if (matchBlue && matchBlue[1]) {
+      game.blue = parseInt(matchBlue[1], 10);
+      session.blue = parseInt(matchBlue[1], 10);
+    }
+
+    // find red
+    const regexRed = /(\d+)\s+red/;
+    const matchRed = regexRed.exec(p);
+
+    if (matchRed && matchRed[1]) {
+      game.red = parseInt(matchRed[1], 10);
+      session.red = parseInt(matchRed[1], 10);
+    }
+
+    // find green
+    const regexGreen = /(\d+)\s+green/;
+    const matchGreen = regexGreen.exec(p);
+
+    if (matchGreen && matchGreen[1]) {
+      game.green = parseInt(matchGreen[1], 10);
+      session.green = parseInt(matchGreen[1], 10);
+    }
+
+    game.sessions?.push(session);
+
+  });
+
+  return game;
 }
 
 export function sum(lines: string[]): number {
 
-  let totalSum: number = 0;
-
+  let possibleGames: Game[] = [];
 
   for (const line of lines) {
-    let games = parse(line);
-    let ignoredIdList: number[] = [];
+    let game = parseLine(line);
 
-    // ignored id list
-    games.forEach(g => { if (evaluate(g) == 0) ignoredIdList.push(g.id) });
-
-    if (ignoredIdList.indexOf(games[0].id) >= 0)
-      continue;
-
-    totalSum += games[0].id;
+    if(game.sessions?.every(s => evaluateSession(s)))
+      possibleGames.push(game); 
+        
   }
-
-  return totalSum;
-
+  return possibleGames.reduce((acc, cur) => acc + cur.id, 0);
 }
 
 
