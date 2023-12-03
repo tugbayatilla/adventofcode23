98aaccb6b548b3758759659af4d489896e5872e0
Tugbay Atilla <tugbayatilla@gmail.com>
2023-12-03 10:43:03 +0100

    refactor(d2p1):introduce session interface

--- src/day2/part1/game.ts
+++ src/day2/part1/game.ts
@@ -3,7 +3,11 @@ export interface Game {
     red?: number | null,
     green?: number| null,
     blue?: number | null,
-    sessions?: {red?: number | null,
-        green?: number| null,
-        blue?: number | null,}[]
+    sessions?: Session[]
+}
+
+export interface Session{
+    red?: number | null,
+    green?: number| null,
+    blue?: number | null,
 }
