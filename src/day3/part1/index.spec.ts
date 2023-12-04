00d1af33fa475643fc39123af53245b05a201987
Tugbay Atilla <tatilla@igus.net>
2023-12-04 16:53:12 +0100

    refactor(d3p1): extract variables

--- src/day3/part1/index.spec.ts
+++ src/day3/part1/index.spec.ts
@@ -17,22 +17,19 @@ describe(`${Day}: the sum of all of the part numbers in the engine schematic`, (
     })
   });
 
-  describe(`${Day}: find neighbors of given index on the line`, () => {
+  describe(`${Day}: find neighbors of given index on the line '467..114..'`, () => {
+    const allLines: string[] = [
+      '467..114..'
+    ]
+    const indexOfLine = 0;
+    
     it("should 'right' neighbor of 467 be . (dot)", () => {
-      const allLines: string[] = [
-        '467..114..'
-      ]
-      const indexOfLine = 0;
-      const direction = 'right'
       const number = 467;
+      const direction = 'right'
       expect(findNeighbor(allLines, indexOfLine, number, direction))
       .to.deep.equal('.');
     }),
     it("should 'left' neighbor of 467 be empty string", () => {
-      const allLines: string[] = [
-        '467..114..'
-      ]
-      const indexOfLine = 0;
       const direction = 'left'
       const number = 467;
       expect(findNeighbor(allLines, indexOfLine, number, direction))
