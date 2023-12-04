0fc07f845817ad2218f31c494dcf305b7162404d
Tugbay Atilla <tatilla@igus.net>
2023-12-04 11:40:57 +0100

    feat(d3p1): initial commit

---
+++ src/day3/part1/index.spec.ts
@@ -0,0 +1,7 @@
+import { expect } from "chai";
+import { Day } from ".";
+
+
+describe(`${Day}: the sum of all of the part numbers in the engine schematic`, () => {
+    expect(true).to.equal(true);
+});
---
+++ src/day3/part1/index.ts
@@ -0,0 +1,6 @@
+export const Day = "day3"; // <-- change this when you copy
+export const SourceFolderPath = `./src/${Day}/part1/`; // <-- change this when you copy
+
+
+    
+
