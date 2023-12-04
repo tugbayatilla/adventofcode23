3a6a3afdfc7d8bfe5767530042fb1b765d162220
Tugbay Atilla <tatilla@igus.net>
2023-12-04 15:01:02 +0100

    feat(d3p1): implement find numbers from a line

--- src/day3/part1/index.spec.ts
+++ src/day3/part1/index.spec.ts
@@ -1,7 +1,10 @@
 import { expect } from "chai";
-import { Day } from ".";
-
+import { Day, findNumbers } from ".";
 
 describe(`${Day}: the sum of all of the part numbers in the engine schematic`, () => {
-    expect(true).to.equal(true);
+  describe(`${Day}: find numbers in a string (line)`, () => {
+    it("should result array contains 467 and 114", () => {
+      expect(findNumbers("467..114..")).to.deep.equal([467, 114]);
+    });
+  });
 });
