ccc666d864e8adcde1367d5e509941778f9f40c1
Tugbay Atilla <tatilla@igus.net>
2023-12-04 11:41:30 +0100

    feat(d3p1): conceptual design - template method

--- src/day3/part1/index.ts
+++ src/day3/part1/index.ts
@@ -1,6 +1,15 @@
 export const Day = "day3"; // <-- change this when you copy
 export const SourceFolderPath = `./src/${Day}/part1/`; // <-- change this when you copy
 
-
+// read(filePath:string): string[] -- line
+// FOREACH line
+    // findNumbers(line:string): number[]
+    // FOREACH number
+        // findIndex(num: number): number
+        // getNeighbor('top'|'botton'|'left','right':string, index: number): string
+        // FOREACH neighbor - 'top'|'botton'|'left','right'
+            // hasSymbol(neighbor: string): boolean 
+            // IF(hasSymbol) 
+                //addSumTotal();
     
 
