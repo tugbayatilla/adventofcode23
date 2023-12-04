e2e91657151fb591fe9088c420a6733d684ff601
Tugbay Atilla <tatilla@igus.net>
2023-12-04 17:18:09 +0100

    refactor(d3p1): use actual test data to finf neighbors

--- src/day3/part1/index.spec.ts
+++ src/day3/part1/index.spec.ts
@@ -1,7 +1,8 @@
 import { expect } from "chai";
-import { Day, findIndex, findNeighbor, findNumbers } from ".";
+import { Day, SourceFolderPath, findIndex, findNeighbor, findNumbers } from ".";
+import { read } from "../../read";
 
-describe(`${Day}: the sum of all of the part numbers in the engine schematic`, () => {
+describe(`${Day}: the sum of all of the part numbers in the engine schematic`, async () => {
   describe(`${Day}: find numbers in a string (line)`, () => {
     it("should result array contains 467 and 114", () => {
       expect(findNumbers("467..114..")).to.deep.equal([467, 114]);
@@ -12,33 +13,33 @@ describe(`${Day}: the sum of all of the part numbers in the engine schematic`, (
     it("should index of 467 be 0", () => {
       expect(findIndex("467..114..", 467)).to.deep.equal(0);
     }),
-    it("should index of 114 be 5", () => {
-      expect(findIndex("467..114..", 114)).to.deep.equal(5);
-    })
+      it("should index of 114 be 5", () => {
+        expect(findIndex("467..114..", 114)).to.deep.equal(5);
+      });
   });
 
-  describe(`${Day}: find neighbors of given index on the line '467..114..'`, () => {
-    const allLines: string[] = [
-      '467..114..'
-    ]
+  let allLines: string[] = [];
+  await read(`${SourceFolderPath}test.data`).then(
+    (lines) => (allLines = lines)
+  );
+
+  describe(`${Day}: find neighbors of given index on the line '${allLines[0]}'`, () => {
     const indexOfLine = 0;
-    const theories: ['right'|'left', number, string][] = [
-      ["right", 467, '.'],
-      ["left", 467, ''],
-      ["right", 114, '.'],
-      ["left", 114, '.']
+    const theories: ["right" | "left", number, string][] = [
+      ["right", 467, "."],
+      ["left", 467, ""],
+      ["right", 114, "."],
+      ["left", 114, "."],
     ];
-  
+
     theories.forEach(([direction, number, expected]) => {
-      it(`should '${direction}' neighbor of 114 be ${expected === '' ? 'empty string' : expected }`, () => {
-        expect(findNeighbor(allLines, indexOfLine, number, direction))
-        .to
-        .deep
-        .equal(expected);
+      it(`should '${direction}' neighbor of ${number} be ${
+        expected === "" ? "empty string" : expected
+      }`, () => {
+        expect(
+          findNeighbor(allLines, indexOfLine, number, direction)
+        ).to.deep.equal(expected);
       });
     });
-
   });
-
-
 });
