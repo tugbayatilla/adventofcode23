ee3e3feb999bfd6022a55d0ecce02b33f8be815d
Tugbay Atilla <tugbayatilla@gmail.com>
2023-12-11 13:59:38 +0100

    test(d3p2): check first item structure

--- src/day3/part2/index.spec.ts
+++ src/day3/part2/index.spec.ts
@@ -1,5 +1,5 @@
 import { expect } from "chai";
-import { DayAndPart, createItems, sum } from ".";
+import { DayAndPart, Item, createItems, sum } from ".";
 
 describe(`${DayAndPart}: Sum of number in a line`, () => {
   it("should sum '467..114..' be 581", () => {
@@ -16,5 +16,17 @@ describe(`${DayAndPart}: create item for each number in a line`, () => {
       '467..114..'
     ];
     expect(createItems(data).length).to.be.equal(2);
+  });
+
+  it("should first item value is 467 on '467..114..' ", () => {
+    const data = [
+      '467..114..'
+    ];
+    expect(createItems(data)[0]).to.be.deep.equal(<Item>{
+      type: 'digit',
+      value: 467,
+      startIndex: 0,
+      endIndex: 2
+    });
   });
 });
