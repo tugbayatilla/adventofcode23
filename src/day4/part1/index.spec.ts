c73db2d803e0b147b375a43bc2359770cc2709aa
Tugbay Atilla <tugbayatilla@gmail.com>
2023-12-13 19:40:23 +0100

    feat(d4p1): initial setup

---
+++ src/day4/part1/index.spec.ts
@@ -0,0 +1,9 @@
+import { expect } from "chai";
+import { DayAndPart } from ".";
+
+
+describe(`${DayAndPart}: `, () => {
+  it("should ", () => {
+    expect(true).to.be.equal(true);
+  });
+});
---
+++ src/day4/part1/index.ts
@@ -0,0 +1,5 @@
+
+const Day = "day4"; // <-- change this when you copy
+const Part = "part1"; // <-- change this when you copy
+export const DayAndPart = `${Day}(${Part[0]}${Part[Part.length - 1]})`;
+export const SourceFolderPath = `./src/${Day}/${Part}/`;
