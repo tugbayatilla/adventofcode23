3f56a3e1ef5d8f2d6ed5d020b5bb1ea3517b99e3
Tugbay Atilla <tugbayatilla@gmail.com>
2023-12-14 08:08:04 +0100

    test(d4p1): convert to theorie

--- src/day4/part1/index.spec.ts
+++ src/day4/part1/index.spec.ts
@@ -3,8 +3,17 @@ import { DayAndPart, sum } from ".";
 
 
 describe(`${DayAndPart}: Single line worthy calculation`, () => {
-  it("should card 1 be worth 8 points", () => {
-    const line = 'Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53'
-    expect(sum(line)).to.be.equal(8);
+  
+  const theories: [line:string, expected:number][] = [
+    ['Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53', 8]
+  ];
+  
+  theories.forEach((theory, index) => {
+    const line = theory[0]
+    const expected = theory[1]
+    it(`should card ${index} be worth ${expected} points`, () => {
+      expect(sum(line)).to.be.equal(expected);
+    });
   });
+  
 });
