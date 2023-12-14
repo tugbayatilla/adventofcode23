93a2c6e1188a833d44ff86f26f1e4c5f145730cd
Tugbay Atilla <tugbayatilla@gmail.com>
2023-12-14 07:55:01 +0100

    feat(d4p1): implement sum for should card 1 be worth 8 points

--- src/day4/part1/index.ts
+++ src/day4/part1/index.ts
@@ -1,3 +1,4 @@
+import { findNumbers } from "../../day3/part1";
 
 const Day = "day4"; // <-- change this when you copy
 const Part = "part1"; // <-- change this when you copy
@@ -29,5 +30,14 @@ export const SourceFolderPath = `./src/${Day}/${Part}/`;
 */
 
 export const sum = (line: string): number => {
-    return 8;
+    const parts = line.split('|');
+    const winners: number[] = findNumbers(parts[0]).slice(1);
+    const numbers: number[] = findNumbers(parts[1]);
+
+    const numbersSet = new Set(numbers);
+    const overlappingWinners = winners.filter(number => numbersSet.has(number));
+
+    const len = overlappingWinners.length;
+    const coefficient = len > 0 ? 1 : 0;
+    return coefficient * Math.pow(2, len - 1);
 };
