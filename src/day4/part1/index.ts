701e2f6116f933e00e1de1b1827146f8a994fb7a
Tugbay Atilla <tugbayatilla@gmail.com>
2023-12-14 07:30:36 +0100

    feat(d4p1): introduce design

--- src/day4/part1/index.ts
+++ src/day4/part1/index.ts
@@ -3,3 +3,27 @@ const Day = "day4"; // <-- change this when you copy
 const Part = "part1"; // <-- change this when you copy
 export const DayAndPart = `${Day}(${Part[0]}${Part[Part.length - 1]})`;
 export const SourceFolderPath = `./src/${Day}/${Part}/`;
+
+
+/*
+    Lets do some design
+
+    interface Card {
+        id: number,
+        winners: number[],
+        numbers: number[]
+    }
+
+    read(filePath): string[]
+    FOREACH line
+    create a card
+    split('|') winners and numbers
+    for winners: 
+        set to card findNumbers(line: string): number[]
+    for numbers
+        set to card findNumbers(line: string): number[]
+    compare winners exist in numbers - findCommon(thisList: number[], inHere: number[]): number[]
+    sum += apply formula: lenght + 2^length
+    
+
+*/
