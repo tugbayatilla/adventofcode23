c543534951104a9cac094623acd77634ce1576b5
Tugbay Atilla <tatilla@igus.net>
2023-12-16 14:38:01 +0100

    feat(d5p1): initial setup

---
+++ src/day5/part1/index.spec.ts
@@ -0,0 +1,11 @@
+import { expect } from "chai";
+import { DayAndPart } from ".";
+
+
+describe(`${DayAndPart}: `, () => {
+  
+    it(`should `, () => {
+      expect(true).to.be.equal(true);
+    });
+  
+});
---
+++ src/day5/part1/index.ts
@@ -0,0 +1,6 @@
+
+const Day = "day5"; // <-- change this when you copy
+const Part = "part1"; // <-- change this when you copy
+export const DayAndPart = `${Day}(${Part[0]}${Part[Part.length - 1]})`;
+export const SourceFolderPath = `./src/${Day}/${Part}/`;
+
