647aae910eb629b4ca0506d98a35cef980771154
Tugbay Atilla <tatilla@igus.net>
2023-12-17 18:39:44 +0100

    feat(d6p1): design

--- src/day6/part1/index.ts
+++ src/day6/part1/index.ts
@@ -5,6 +5,17 @@ export const identity = new Identity(6, 1);
 
 /*
 
+    
+    interface Race {
+        time: number,
+        distance: number
+    }
 
+    - Parse data to Race[]
+    - FOREACH race
+        - FOREACH race.time
+            - calculate distance calculateDistance(duration:number, race: Race): number
+        - increase the counter for the race
+    - multiply the couters for each race
 
 */
