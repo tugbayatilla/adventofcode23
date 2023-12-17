1cb974c259455c4405f1b09970b25c89ba0f4125
Tugbay Atilla <tatilla@igus.net>
2023-12-17 19:14:09 +0100

    feat(d6p2): implementation and submit answer

--- src/day6/part2/index.ts
+++ src/day6/part2/index.ts
@@ -7,29 +7,30 @@ export const identity = new Identity(6, 2);
 
 export async function answer(filePath: string): Promise<number> {
     return read(filePath).then((lines) => {
-        
+
         const times = findNumbers(lines[0]);
         const distances = findNumbers(lines[1]);
 
-        let result = 1;
-        for (let t = 0; t < times.length; t++) {
-            let timeCounter = 0;
-            const time = times[t];
-            
-            for (let ms = 0; ms <= time; ms++) {
-                const distance = (time - ms) * ms;
-                if(distance > distances[t])
-                    timeCounter++;
-            }
-            result *= timeCounter;
+        const time = Number(times.join(''))
+        const dist = Number(distances.join(''))
+        
+        let timeCounter = 0;
+        for (let ms = 0; ms <= time; ms++) {
+            const distance = (time - ms) * ms;
+            if (distance > dist)
+                timeCounter++;
         }
 
-        return result;
+        return timeCounter;
 
     });
 }
 
 
-if (isIdentitySelected(identity))
-answer(identity.getPuzzlePath())
-.then((sum) => console.log(`(${identity.show}): ${sum}`))
+if (isIdentitySelected(identity)) {
+    answer(identity.getTestPath())
+        .then((sum) => console.log(`(${identity.show}): ${sum} - Test Data`))
+
+    answer(identity.getPuzzlePath())
+        .then((sum) => console.log(`(${identity.show}): ${sum} - Puzzle Data`))
+}
