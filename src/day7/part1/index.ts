4a93fb98da8989642c0ef37936b8fe27b98ca38b
Tugbay Atilla <tugbayatilla@gmail.com>
2023-12-18 19:27:17 +0100

    feat(d7p1): try #2 with test data without tests - 6440/6440

--- src/day7/part1/index.ts
+++ src/day7/part1/index.ts
@@ -40,7 +40,7 @@ const CARDS: string[] = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', 
 export async function answer(filePath: string): Promise<number> {
     return read(filePath).then((lines) => {
 
-        let handAndPower: [hand:string, bid:number, power:number][] = [];
+        let handAndPower: [hand: string, bid: number, power: number][] = [];
 
         for (const line of lines) {
             const lineSplit = line.split(' ');
@@ -48,54 +48,63 @@ export async function answer(filePath: string): Promise<number> {
             const bid = Number(lineSplit[1]);
 
             let handPower = 0;
-            [...hand].forEach(card => {
 
-                const cardIndex = CARDS.indexOf(card);
-                const cardLocationPower = CARDS.length - cardIndex;
-
-                handPower += cardLocationPower;
-            });
 
             const handLabelCount = findHandLabelCounts(hand);
 
             const fiveOfAKind = handLabelCount.filter(p => p[1] === 5);
             if (fiveOfAKind.length === 1) {
-                handPower += 2 ** 7;
+                handPower += 1000000000;
             }
             const fourOfAKind = handLabelCount.filter(p => p[1] === 4);
             if (fourOfAKind.length === 1) {
-                handPower += 2 ** 6;
+                handPower += 900000000;
             }
-            const fullHause = handLabelCount.filter(p => p[1] === 2 || p[1] === 3);
-            if (fullHause.length === 2) {
-                handPower += 2 ** 5;
+            const fullHause2 = handLabelCount.filter(p => p[1] === 2);
+            const fullHause3 = handLabelCount.filter(p => p[1] === 3);
+            if (fullHause2.length === 1 && fullHause3.length === 1) {
+                handPower += 800000000;
             }
             const threeOfAKind = handLabelCount.filter(p => p[1] === 3);
             if (threeOfAKind.length === 1) {
-                handPower += 2 ** 4;
+                handPower += 700000000;
             }
             const twoPairs = handLabelCount.filter(p => p[1] === 2);
             if (twoPairs.length === 2) {
-                handPower += 2 ** 3;
+                handPower += 600000000;
             }
             const onePair = handLabelCount.filter(p => p[1] === 2);
             if (onePair.length === 1) {
-                handPower += 2 ** 2;
+                handPower += 500000000;
             }
             const highNumber = handLabelCount.filter(p => p[1] === 1);
             if (highNumber.length === 5) {
-                handPower += 2 ** 2;
+                handPower += 400000000;
             }
+
+            [...hand].forEach((card, index) => {
+
+                const cardIndex = CARDS.indexOf(card);
+                const cardLocationPower = (CARDS.length - cardIndex) ** ([...hand].length - index);
+
+                handPower += cardLocationPower;
+            });
+
+            console.log([hand, bid, handPower]);
 
             handAndPower.push([hand, bid, handPower]);
 
         }
 
-        const sortedHandAndPower = handAndPower.sort((a,b)=> a[2]-b[2]); //desc
+        const sortedHandAndPower = handAndPower.sort((a, b) => a[2] - b[2]); //desc
+        console.log('sorted');
+        console.log(sortedHandAndPower);
 
-        let sum=0;
-        sortedHandAndPower.forEach((item,index)=>{
-            sum += item[1] * (index+1)
+
+        let sum = 0;
+        sortedHandAndPower.forEach((item, index) => {
+            console.log(`${item[1]} * ${index + 1}`)
+            sum += item[1] * (index + 1)
         });
 
         return sum;
@@ -104,18 +113,16 @@ export async function answer(filePath: string): Promise<number> {
 
 function findHandLabelCounts(input: string): [label: string, count: number][] {
     const counts: [string, number][] = [];
-    const regex = /\d/g;
-    let match: RegExpExecArray | null;
 
-    while ((match = regex.exec(input)) !== null) {
-        const label = match[0];
-        let labelAndCount = counts.find(p => p[0] === label)
+    [...input].forEach(char => {
+
+        let labelAndCount = counts.find(p => p[0] === char)
         if (labelAndCount) {
             labelAndCount[1]++;
         } else {
-            counts.push([label, 1]);
+            counts.push([char, 1]);
         }
-    }
+    });
 
     return counts;
 }
