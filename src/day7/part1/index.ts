fc013986509f0b1a9f604fedac400ec32c513f25
Tugbay Atilla <tugbayatilla@gmail.com>
2023-12-18 14:29:02 +0100

    docs(d7p1): design

--- src/day7/part1/index.ts
+++ src/day7/part1/index.ts
@@ -6,7 +6,33 @@ export const identity = new Identity(7, 1);
 
 /*
 
-   
+   interface Hand {
+    text: string,
+    bid: number,
+    type?: HandType,
+    typePower?: number
+    cardPower?: number
+   }
+   interface Card {
+    label: CardLabel,
+    location: number,
+    power: number
+   }
+
+   type HandType = {name: 'Five of a kind', power: 8} | 
+   type CardLabel = 'A' | 'K'
+   const LABELS: string[] = ['A', 'K']
+
+   getLabelPower(label: CardLabel): number {
+    return length - index
+   }
+
+   - findHands(lines: string[]):Hand[] {}
+   - findHandType(hand: Hand): HandType {}
+   - Arrange CardLabel and its power map
+   - Arrange HandType and its power map
+   - findHandPower(hand: Hand): number {}
+
 
 */
 
