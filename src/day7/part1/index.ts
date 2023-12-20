b8dc12659982babdb1357158de3d9ea06fb46646
Tugbay Atilla <tugbayatilla@gmail.com>
2023-12-20 08:26:43 +0100

    feat(d7p1): try 8 times with puzzle data without tests - stop working with adventofcode

--- src/day7/part1/index.ts
+++ src/day7/part1/index.ts
@@ -37,6 +37,7 @@ export const identity = new Identity(7, 1);
 */
 
 const CARDS: string[] = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2'];
+const CARDS_VALUES: number[] = [14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2];
 export async function answer(filePath: string): Promise<number> {
     return read(filePath).then((lines) => {
 
@@ -47,45 +48,18 @@ export async function answer(filePath: string): Promise<number> {
             const hand = lineSplit[0];
             const bid = Number(lineSplit[1]);
 
-            let handPower = 0;
-
-
+        
             const handLabelCount = findHandLabelCounts(hand);
 
-            const fiveOfAKind = handLabelCount.filter(p => p[1] === 5);
-            if (fiveOfAKind.length === 1) {
-                handPower += 1000000000;
-            }
-            const fourOfAKind = handLabelCount.filter(p => p[1] === 4);
-            if (fourOfAKind.length === 1) {
-                handPower += 900000000;
-            }
-            const fullHause2 = handLabelCount.filter(p => p[1] === 2);
-            const fullHause3 = handLabelCount.filter(p => p[1] === 3);
-            if (fullHause2.length === 1 && fullHause3.length === 1) {
-                handPower += 800000000;
-            }
-            const threeOfAKind = handLabelCount.filter(p => p[1] === 3);
-            if (threeOfAKind.length === 1) {
-                handPower += 700000000;
-            }
-            const twoPairs = handLabelCount.filter(p => p[1] === 2);
-            if (twoPairs.length === 2) {
-                handPower += 600000000;
-            }
-            const onePair = handLabelCount.filter(p => p[1] === 2);
-            if (onePair.length === 1) {
-                handPower += 500000000;
-            }
-            const highNumber = handLabelCount.filter(p => p[1] === 1);
-            if (highNumber.length === 5) {
-                handPower += 400000000;
-            }
+            let handPower = apple(handLabelCount);
 
             [...hand].forEach((card, index) => {
 
                 const cardIndex = CARDS.indexOf(card);
-                const cardLocationPower = (CARDS.length - cardIndex) ** ([...hand].length - index);
+                const getValue = CARDS_VALUES[cardIndex];
+                // const cardLocationPower = getValue ** ([...hand].length - index) + (CARDS.length + [...hand].length - index);
+                const cardPosPower = [...hand].length - index;
+                const cardLocationPower = cardPosPower * getValue ** cardPosPower;
 
                 handPower += cardLocationPower;
             });
@@ -103,12 +77,47 @@ export async function answer(filePath: string): Promise<number> {
 
         let sum = 0;
         sortedHandAndPower.forEach((item, index) => {
-            console.log(`${item[1]} * ${index + 1}`)
             sum += item[1] * (index + 1)
+            console.log(`${item} - ${index + 1} -- ${sum}`)
         });
 
         return sum;
     });
+}
+
+function apple(handLabelCount: [label: string, count: number][]) {
+    
+    const fiveOfAKind = handLabelCount.filter(p => p[1] === 5);
+    if (fiveOfAKind.length === 1) {
+        return 1000000000000;
+    }
+    const fourOfAKind = handLabelCount.filter(p => p[1] === 4);
+    if (fourOfAKind.length === 1) {
+        return 900000000000;
+    }
+    const fullHause2 = handLabelCount.filter(p => p[1] === 2);
+    const fullHause3 = handLabelCount.filter(p => p[1] === 3);
+    if (fullHause2.length === 1 && fullHause3.length === 1) {
+        return 800000000000;
+    }
+    const threeOfAKind = handLabelCount.filter(p => p[1] === 3);
+    if (threeOfAKind.length === 1) {
+        return 700000000000;
+    }
+    const twoPairs = handLabelCount.filter(p => p[1] === 2);
+    if (twoPairs.length === 2) {
+        return 600000000000;
+    }
+    const onePair = handLabelCount.filter(p => p[1] === 2);
+    if (onePair.length === 1) {
+        return 500000000000;
+    }
+    const highNumber = handLabelCount.filter(p => p[1] === 1);
+    if (highNumber.length === 5) {
+        return 400000000000;
+    }
+
+    return -1;
 }
 
 function findHandLabelCounts(input: string): [label: string, count: number][] {
@@ -127,6 +136,18 @@ function findHandLabelCounts(input: string): [label: string, count: number][] {
     return counts;
 }
 
-if (isIdentitySelected(identity))
+if (isIdentitySelected(identity)) {
     answer(identity.getTestPath())
-        .then((sum) => console.log(`(${identity.show}): ${sum}`))
+        .then((sum) => console.log(`(${identity.show}): ${sum} - test`))
+
+        answer(identity.getPuzzlePath())
+        .then((sum) => console.log(`(${identity.show}): ${sum} puzzle`))
+// 251232593 too low
+// 251412864 
+// 251983066 too high
+// 251412864 wrong answer
+// 251405536 wrong
+// 251674863 wrong
+// 251405536 wrong
+// 251384246 wrong
+}
