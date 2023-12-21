bb386bde699a1f2d328e3e47fc1cf795a5b7dbf5
Tugbay Atilla <tugbayatilla@gmail.com>
2023-12-21 20:43:52 +0100

    feat(d8p1): convert string network to data set

--- src/day8/part1/index.ts
+++ src/day8/part1/index.ts
@@ -1,14 +1,38 @@
 import { Identity, isIdentitySelected } from "../../CurrentDayAndPart";
 import { read } from "../../read";
+import { write } from "../../write";
 
 export const identity = new Identity(8, 1);
+
+interface data {
+    name: string,
+    direction: [string, string]
+}
 
 export async function answer(filePath: string): Promise<number> {
     return read(filePath).then((lines) => {
 
-        const directionsStr = lines[0];
-        const directions: number[] = [...directionsStr].map(p=> p === 'L' ? 0 : 1);
-    
+        //const directionsStr = lines[0];
+        //const directions: number[] = [...directionsStr].map(p=> p === 'L' ? 0 : 1);
+
+
+        const dataSet: data[] = [];
+        lines.splice(1).forEach(line => {
+
+            const regex = new RegExp('(\\w+)\\s*=\\s*\\((\\w+),\\s*(\\w+)\\)');
+            const matches = line.match(regex);
+
+
+            if (matches) {
+                const data1: data = { name: matches[1], direction: [matches[2], matches[3]] };
+
+                dataSet.push(data1);
+            }
+        });
+
+        write(`${filePath}.out`, dataSet.map(p => JSON.stringify(p)));
+
+
 
         return 0;
     });
