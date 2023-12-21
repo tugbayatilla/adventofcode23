a8b502ee317c4b60d1f99f03576f0db00f505e60
Tugbay Atilla <tugbayatilla@gmail.com>
2023-12-21 20:21:54 +0100

    feat(d8p1): setup

---
+++ src/day8/part1/index.ts
@@ -0,0 +1,19 @@
+import { Identity, isIdentitySelected } from "../../CurrentDayAndPart";
+import { read } from "../../read";
+
+export const identity = new Identity(8, 1);
+
+export async function answer(filePath: string): Promise<number> {
+    return read(filePath).then((lines) => {
+
+        return 0;
+    });
+}
+
+if (isIdentitySelected(identity)) {
+    answer(identity.getTestPath())
+        .then((sum) => console.log(`(${identity.show}): ${sum} - test`))
+
+    answer(identity.getPuzzlePath())
+        .then((sum) => console.log(`(${identity.show}): ${sum} - puzzle`))
+}
